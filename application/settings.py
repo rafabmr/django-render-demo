97a1cf40ca47e41cf37159b8ea44a316be242e65
rafabmr <iuninefrendor@gmail.com>
2022-09-09 13:16:02 -0500

    Demo 3

--- application/settings.py
+++ application/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d+$$0auv_!zjyonxlqy&7q@v*sfz3oasn8exp1%u)#_rhnfcr1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
